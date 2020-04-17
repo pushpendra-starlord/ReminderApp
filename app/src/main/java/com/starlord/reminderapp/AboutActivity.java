9038d8e644c7d598bf61b14a91dc6cb8cbec89ac
pushpendra-starlord <pushpendratripathi@outlook.com>
2020-04-17 19:45:44 +0530

    Added Splash Screen and About Info

--- app/src/main/java/com/starlord/reminderapp/AboutActivity.java
+++ app/src/main/java/com/starlord/reminderapp/AboutActivity.java
@@ -2,14 +2,24 @@ package com.starlord.reminderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
-
+    Button home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         getSupportActionBar().setTitle("About");
+        home = findViewById(R.id.btnHome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+            }
+        });
     }
 }
